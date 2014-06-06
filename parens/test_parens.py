b8d89659508541b1a133143de1943353f12a7bab
John Shiver <johneshiver@gmail.com>
2014-06-06 12:03:45 -0700

    update test_parens

--- parens/test_parens.py
+++ parens/test_parens.py
@@ -3,20 +3,20 @@ import parens
 
 def test_open_parens():
     myString = "(a+b)*((x/2)"
-    assert check_parens(myString) == 1
+    assert parens.check_parens(myString) == 1
     myString = "(((((((()))(((())))"
-    assert check_parens(myString) == 1
+    assert parens.check_parens(myString) == 1
 
 def test_broken_parens():
     myString = "(a=b)*'this'+)/(9)"
-    assert check_parens(myString) == -1
+    assert parens.check_parens(myString) == -1
     myString = ")(((())))"
-    assert check_parens(myString) == -1
+    assert parens.check_parens(myString) == -1
     myString = "(()))(((()))))"
-    assert check_parens(myString) == -1
+    assert parens.check_parens(myString) == -1
 
 def test_balanced_parens():
     myString = "(a+b*(3y))"
-    assert check_parens(myString) == 0
+    assert parens.check_parens(myString) == 0
     myString = "(((((((())))))))(((())))((((()))))"
-    assert check_parens(myString) == 0
+    assert parens.check_parens(myString) == 0
