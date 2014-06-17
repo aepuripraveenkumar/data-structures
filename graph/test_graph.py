3fbb7329d1a5d0bbd97fc0bbc8b34508f2a0adf1
lfritts <llfritts@gmail.com>
2014-06-17 13:24:34 -0700

    Add test for graph init

--- graph/test_graph.py
+++ graph/test_graph.py
@@ -1,6 +1,13 @@
 from graph import Node
+from graph import Graph
 
 
 def test_init_node():
     newNode = Node("Decatur")
     assert newNode.value == "Decatur"
+
+
+def test_init_graph():
+    newGraph = Graph()
+    assert newGraph.nodelist is None
+    assert newGraph.edgelist is None
