9818f16c551a7c988a17d268561166f4b4ac8ffe
lfritts <llfritts@gmail.com>
2014-06-17 13:35:14 -0700

    Add test for init graph class

--- graph/test_graph.py
+++ graph/test_graph.py
@@ -1,5 +1,6 @@
 from graph import Node
 from graph import Graph
+from graph import Edge
 
 
 def test_init_node():
@@ -11,3 +12,17 @@ def test_init_graph():
     newGraph = Graph()
     assert len(newGraph.nodelist) == 0
     assert len(newGraph.edgelist) == 0
+
+
+def test_init_edge():
+    newEdge = Edge()
+    assert len(newEdge.nodes) == 0
+
+
+def test_init_edge_w_nodes():
+    node1 = Node("1st")
+    node2 = Node("2nd")
+    newEdge = Edge(node1, node2)
+    assert len(newEdge.nodes) == 2
+    assert newEdge.nodes[0].value == "1st"
+    assert newEdge.nodes[1].value == "2nd"
