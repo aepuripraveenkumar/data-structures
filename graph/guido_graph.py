dd5eec5b7c73a4ab4f023eb9ebeb303eb69205f7
John Shiver <johneshiver@gmail.com>
2014-06-18 23:06:05 -0700

    Add add edge, del node, add node methods and tests

--- graph/guido_graph.py
+++ graph/guido_graph.py
@@ -6,55 +6,58 @@
          'E': ['F'],
          'F': ['C']}"""
 
-graph = {}
 
+class Graph(object):
+    def __init__(self):
+        self.graph = {}
 
-def nodes():
-    """return list of all nodes in graph"""
-    return graph.keys()
+    def nodes(self):
+        """return list of all nodes in graph"""
+        return self.graph.keys()
 
+    def edges(self):
+        """return list of all edges in graph"""
+        return [(i, e) for i in self.graph.keys() for e in self.graph[i]]
 
-def edges():
-    """return list of all edges in graph"""
-    pass
+    def add_node(self, n):
+        """adds new node 'n' to graph"""
+        if n not in self.graph.keys():
+            self.graph[n] = []
 
+    def add_edge(self, n1, n2):
+        """"adds new edge to graph connecting n1 and n2
+            if either n1 or n2 are not already in graph, adds them"""
+        nodes = self.graph.keys()
+        if n1 not in nodes:
+            self.graph[n1] = []
+        if n2 not in nodes:
+            self.graph[n2] = []
+        self.graph[n1].append(n2)
 
-def add_node():
-    """adds new node 'n' to graph"""
-    pass
+    def del_node(self, n):
+        """deletes node n from graph
+           raises error if no such node exists"""
+        if n in self.graph.keys():
+            self.graph.remove(n)
+        else:
+            raise ValueError
 
+    def del_edge(self, n1, n2):
+        """deletes edge containing n1 and n2 from graph
+           raises error if no such node exists"""
+        pass
 
-def add_edge(n1, n2):
-    """"adds new edge to graph connecting n1 and n2
-        if either n1 or n2 are not already in graph, adds them"""
-    pass
+    def has_node(self, n):
+        """True if ndoe n is contained in graph, otherwise False"""
+        pass
 
+    def neighbors(self, n):
+        """Returns list of all nodes connected to n by edges
+           raises an error if n is not in g"""
+        pass
 
-def del_node(n):
-    """deletes node n from graph
-       raises error if no such node exists"""
-    pass
-
-
-def del_edge(n1, n2):
-    """deletes edge containing n1 and n2 from graph
-       raises error if no such node exists"""
-    pass
-
-
-def has_node(n):
-    """True if ndoe n is contained in graph, otherwise False"""
-    pass
-
-
-def neighbors(n):
-    """Returns list of all nodes connected to n by edges
-       raises an error if n is not in g"""
-    pass
-
-
-def adjacent(n1, n2):
-    """returns True if there is an edge connecting n1 and n2
-       False if not. Raises an error if either the supplied
-       nodes not in G"""
-    pass
+    def adjacent(self, n1, n2):
+        """returns True if there is an edge connecting n1 and n2
+           False if not. Raises an error if either the supplied
+           nodes not in G"""
+        pass
--- graph/test_guido.py
+++ graph/test_guido.py
@@ -1,11 +1,29 @@
 import pytest
-from guido_graph import graph
-from guido_graph import nodes
-from guido_graph import edges
-from guido_graph import add_node
+from guido_graph import Graph
 
 
-def test_return_nodes():
-    graph['A'] = []
-    graph['B'] = []
-    assert nodes() == ['A', 'B']
+def test_add_node():
+    graph1 = Graph()
+    graph1.add_node('A')
+    graph1.add_node('B')
+    assert graph1.nodes() == ['A', 'B']
+
+
+def test_add_existing_node():
+    graph1 = Graph()
+    graph1.add_node('A')
+    graph1.add_node('A')
+    assert graph1.nodes() == ['A']
+
+
+def test_add_edge():
+    graph1 = Graph()
+    graph1.add_node('A')
+    graph1.add_node('B')
+    graph1.add_edge('A', 'B')
+    assert graph1.edges() == [('A', 'B')]
+
+
+# def test_add_edge_no_node():
+#     add_edge('A', 'B')
+#     assert edges() == [('A', 'B')]
