80bf502537a50373bdf804a2f10637f042b885bb
lfritts <llfritts@gmail.com>
2014-07-13 11:29:25 -0700

    Add HashTable class

---
+++ hash/hashtable.py
@@ -0,0 +1,21 @@
+
+class HashTable(object):
+
+    def __init__(self, size):
+        self.size = size
+        self.hash_t = dict((str(i), {}) for i in range(self.size))
+
+    def hash(self, key):
+        index = None
+        for ch in key:
+            index += ord(ch)
+        return index % self.size
+
+    def set(self, key, value):
+        self.hash_t[self.hash(key)][key] = value
+
+    def get(self, key):
+        index = self.hash(key)
+        if key in self.hash_t[index].keys():
+            return self.hash_t[index][key]
+        raise KeyError
